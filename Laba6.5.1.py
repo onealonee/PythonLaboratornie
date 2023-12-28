24854d1d25ed7ab1b42d1affcbb7f232e52df016
onealonee <kazimiov.sk.xx@mail.ru>
2023-12-28 11:34:10 +0300

    1

---
+++ Laba6.5.1.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# Ввод размерности матрицы от пользователя
+n = int(input("Введите размерность квадратной матрицы: "))
+
+# Генерация псевдослучайных элементов матрицы
+matrix = np.random.rand(n, n)
+
+# Вывод исходной матрицы
+print("Исходная матрица:")
+print(matrix)
+
+# Нахождение обратной матрицы
+try:
+    inverse_matrix = np.linalg.inv(matrix)
+
+    # Вывод обратной матрицы
+    print("\nОбратная матрица:")
+    print(inverse_matrix)
+
+except np.linalg.LinAlgError:
+    print("\nОбратной матрицы не существует.")
